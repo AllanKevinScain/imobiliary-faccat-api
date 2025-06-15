3e4db2c0a73b68688ee022673954326242401408
vitorotto <vitorotto05@gmail.com>
2025-06-15 18:44:21 -0300

    ADD: Formulários para criação de Funcionarios, Clientes, Imoveis e Reservas

---
+++ imobiliaria/forms.py
@@ -0,0 +1,51 @@
+from django import forms
+from .models import Funcionario, Cliente, Imovel, Quarto, Reserva
+
+# Formulário para Funcionario
+
+
+class FuncionarioForm(forms.ModelForm):
+    class Meta:
+        model = Funcionario  # Aqui vamos dizer qual modelo esse formulário vai usar
+        fields = ['nome', 'email', 'telefone', 'cargo']
+        # Aqui vamos dizer quais campos do modelo funcionario queremos usar no formulário
+
+# Formulário para Cliente
+
+
+class ClienteForm(forms.ModelForm):
+    data_nascimento = forms.DateField(
+        widget=forms.DateInput(attrs={'type': 'date'}))
+
+    class Meta:
+        model = Cliente
+        fields = ['nome', 'email', 'telefone', 'cpf', 'data_nascimento']
+
+# Formulário para Imovel
+
+
+class ImovelForm(forms.ModelForm):
+    class Meta:
+        model = Imovel
+        fields = ['nome', 'tipo', 'endereco', 'cidade', 'estado', 'disponivel']
+
+# Formulário para Quarto
+
+
+class QuartoForm(forms.ModelForm):
+    class Meta:
+        model = Quarto
+        fields = ['imovel', 'nome']
+
+# Formulário para Reserva
+
+
+class ReservaForm(forms.ModelForm):
+    class Meta:
+        model = Reserva
+        fields = ['imovel', 'cliente',
+                  'funcionario', 'data_inicio', 'data_fim']
+        widgets = {
+            'data_inicio': forms.DateInput(attrs={'type': 'date'}),
+            'data_fim': forms.DateInput(attrs={'type': 'date'}),
+        }
--- imobiliaria/views.py
+++ imobiliaria/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from .models import Funcionario, Cliente, Imovel, Quarto, Reserva
+from .forms import FuncionarioForm, ClienteForm, ImovelForm, QuartoForm, ReservaForm
+from django.shortcuts import redirect
 
 # Create your views here.
+
+
 def index(request):
     return render(request, 'index.html')
+
+# FUNCIONARIOS
+
 
 def funcionarios(request):
     funcionarios = Funcionario.objects.all()
@@ -14,11 +21,27 @@ def funcionarios(request):
 
     return render(request, 'funcionarios/index.html', dados)
 
+
 def funcionarios_cadastrar(request):
-    return render(request, 'funcionarios/funcionarios_cadastrar.html')
+    if request.method == 'POST':
+        form = FuncionarioForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('funcionarios')
+    else:
+        # form vai receber um formulário FuncionarioForm que criamos no forms.py
+        form = FuncionarioForm()
+    dados = {
+        'form': form,
+    }
+    return render(request, 'funcionarios/funcionarios_cadastrar.html', dados)
+
 
 def funcionarios_editar(request):
     return render(request, 'funcionarios/index.html')
+
+# IMOVEIS
+
 
 def imoveis(request):
     imoveis = Imovel.objects.all()
@@ -29,11 +52,26 @@ def imoveis(request):
 
     return render(request, 'imoveis/index.html', dados)
 
+
 def imoveis_cadastrar(request):
-    return render(request, 'imoveis/imoveis_cadastrar.html')
+    if request.method == 'POST':
+        form = ImovelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('imoveis')
+    else:
+        form = ImovelForm()  # form vai receber um formulário ImovelForm que criamos no forms.py
+    dados = {
+        'form': form,
+    }
+    return render(request, 'imoveis/imoveis_cadastrar.html', dados)
+
 
 def imoveis_editar(request):
     return render(request, 'imoveis/index.html')
+
+# CLIENTES
+
 
 def clientes(request):
     clientes = Cliente.objects.all()
@@ -42,13 +80,28 @@ def clientes(request):
         'clientes': clientes,
     }
 
-    return render(request, 'clientes/clientes_cadastrar.html', dados)
+    return render(request, 'clientes/index.html', dados)
+
 
 def clientes_cadastrar(request):
-    return render(request, 'clientes/index.html')
+    if request.method == 'POST':
+        form = ClienteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('clientes')
+    else:
+        form = ClienteForm()  # form vai receber um formulário ClienteForm que criamos no forms.py
+    dados = {
+        'form': form,
+    }
+    return render(request, 'clientes/clientes_cadastrar.html', dados)
+
 
 def clientes_editar(request):
     return render(request, 'clientes/index.html')
+
+# RESERVAS
+
 
 def reservas(request):
     reservas = Reserva.objects.all()
@@ -59,8 +112,20 @@ def reservas(request):
 
     return render(request, 'reservas/index.html', dados)
 
+
 def reservas_cadastrar(request):
-    return render(request, 'reservas/reservas_cadastrar.html')
+    if request.method == 'POST':
+        form = ReservaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('reservas')
+    else:
+        form = ReservaForm()  # form vai receber um formulário ReservaForm que criamos no forms.py
+    dados = {
+        'form': form,
+    }
+    return render(request, 'reservas/reservas_cadastrar.html', dados)
+
 
 def reservas_editar(request):
     return render(request, 'reservas/index.html')
